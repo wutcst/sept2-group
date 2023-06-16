415a9fbc66f6d32d8527d5cf838f937003d7620e
tpiece <105487531+tpiece@users.noreply.github.com>
2023-06-16 18:36:43 +0800

    Update Main.java

--- src/main/java/cn/edu/whut/sept/zuul/Main.java
+++ src/main/java/cn/edu/whut/sept/zuul/Main.java
@@ -1,9 +1,15 @@
 package cn.edu.whut.sept.zuul;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-        Game game = new Game();
-        game.play();
+        //Game game = new Game();
+        //game.play();
+        SpringApplication.run(Main.class, args);
     }
 }
+
