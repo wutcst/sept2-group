abde4b867609b8a57e2a96d765fd3456da37cbce
tpiece <105487531+tpiece@users.noreply.github.com>
2023-06-16 20:15:29 +0800

    玩家类

--- src/main/java/cn/edu/whut/sept/zuul/Entity/Player.java
+++ src/main/java/cn/edu/whut/sept/zuul/Entity/Player.java
@@ -15,11 +15,13 @@ public class Player {
         this.maxWeight=maxWeight;
     }
 
+    //玩家丢弃物品
     public void drop(Room room, int i){
         int w = PlayerWeights.remove(i);
         room.addWeight(w);
     }
 
+    //玩家拿去物品
     public boolean take(Room room,int n){
         int sum=0;
         for(int i=0;i<PlayerWeights.size();i++){
@@ -33,12 +35,14 @@ public class Player {
         }
     }
 
+    //玩家吃魔法饼干
     public void eatMagicCookie(Room room){
         maxWeight+=room.getCookie();
         room.removeCookie();
         room.setCookie(0);
     }
 
+    //背包剩余容量
     public int freeSpace(){
         int sum=0;
         for(int i=0;i<PlayerWeights.size();i++){
