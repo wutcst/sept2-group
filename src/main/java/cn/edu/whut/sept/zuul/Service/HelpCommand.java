a34722e7fe1733aa2169d33499d592dc5e70d426
tpiece <105487531+tpiece@users.noreply.github.com>
2023-06-16 19:54:28 +0800

    Update HelpCommand.java

--- src/main/java/cn/edu/whut/sept/zuul/Service/HelpCommand.java
+++ src/main/java/cn/edu/whut/sept/zuul/Service/HelpCommand.java
@@ -1,5 +1,8 @@
-package cn.edu.whut.sept.zuul;
+package cn.edu.whut.sept.zuul.Service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class HelpCommand extends Command
 {
     private CommandWords commandWords;
